c78fe872e4500eccab3cd1cb0954a3d24efe8b02
Mr-Maxraw <maxraw1@yandex.ru>
2022-03-23 15:35:56 +0300

    basic inserts

--- dbcommands/insert.py
+++ dbcommands/insert.py
@@ -1,25 +1,23 @@
-import duckdb
+def to_job(con, data):
+    con.execute('insert into job values (?, ?, ?, ?, ?)', data)
 
-def to_job(data):
-    pass
+def to_person(con, data):
+    con.execute('insert into job values (?, ?)', data)
 
-def to_person(data):
-    pass
+def to_school(con, data):
+    con.execute('insert into job values (?, ?, ?)', data)
 
-def to_school(data):
-    pass
+def to_rating(con, data):
+    con.execute('insert into job values (?, ?, ?)', data)
 
-def to_rating(data):
-    pass
+def to_pers_data(con, data):
+    con.execute('insert into job values (?, ?, ?)', data)
 
-def to_pers_data(data):
-    pass
+def to_pay(con, data):
+    con.execute('insert into job values (?, ?, ?)', data)
 
-def to_pay(data):
-    pass
+def to_load(con, data):
+    con.execute('insert into job values (?, ?, ?, ?)', data)
 
-def to_load(data):
-    pass
-
-def to_job_title_desc(data):
-    pass
+def to_job_title_desc(con, data):
+    con.execute('insert into job values (?, ?, ?, ?)', data)

e83929efe14baf4911f18bfade2046493171d891
Mr-Maxraw <maxraw1@yandex.ru>
2022-03-29 14:23:46 +0300

    now with ipynb files

--- temp.py
+++ temp.py
@@ -1,19 +1,22 @@
-a = {1: 'asd', 2: 'qwe'}
+# a = {1: 'asd', 2: 'qwe'}
 
-print(a[1])
-print(a[2])
-a['qweiuer'] = 1
-print(a['qweiuer'])
-print(a)
+# print(a[1])
+# print(a[2])
+# a['qweiuer'] = 1
+# print(a['qweiuer'])
+# print(a)
 
-x = 1
-y = 2
-print(type(x), type(a))
-x, y = y, x
-print(x , y)
-z = x, y
-print(type(z))
-print(z)
-y, x = z
-print(f'z is: {z[0]}, {z[1]}')
-print(x, y)
+# x = 1
+# y = 2
+# print(type(x), type(a))
+# x, y = y, x
+# print(x , y)
+# z = x, y
+# print(type(z))
+# print(z)
+# y, x = z
+# print(f'z is: {z[0]}, {z[1]}')
+# print(x, y)
+a = [1, 1, 2, [1,1]]
+b = set(a)
+print(b)

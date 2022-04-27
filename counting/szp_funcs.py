a5e72c31197ee8bfbdd45e359d5ef8cc33531d23
Mr-Maxraw <maxraw1@yandex.ru>
2022-04-27 16:19:13 +0300

    27.04.2022 end day

---
+++ counting/szp_funcs.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows as df_to_row
+
+path_to_docs = 'C:/Users/PetukhovMD/Desktop/справочники/'
+path_to_data = 'C:/Users/PetukhovMD/Desktop/szp_2022/'
+gku = ["ГБУ МГДУ","ГАОУ ДПО ЦПМ","ГАОУ ДПО МЦКО","ГКУ ЦФО ДОНМ","ГБУ ГППЦ ДОНМ","ГКУ СФК ДОНМ","ГКУ Дирекция ДОНМ","ГБОУ ДПО МЦПС","ГБОУ ГМЦ ДОНМ","ГКУ Дирекция по строительству и реконструкции ДОНМ","ГАОУ ДПО ""Корпоративный университет""","ГАУ ""Центр цифровизации образования""","ГАУ Медиацентр","ГАОУ ДПО МЦРПО"]
+gku_inn = [7702061938, 7725618950, 7725539709, 9718071371, 7726317748, 7704191153, 9705101759, 7719210793, 7705399348, 7705020295, 7714239823, 7727190237, 7718924940, 7707329480]
+
+def get_otv():
+    otv = pd.read_excel(path_to_docs + 'Ответственные.xlsx').rename(columns={'инн орг': 'inn', 'округ': 'okr', 'Зарпл. Обслуживание': 'otv', 'Наименование': 'sch_name'})
+    otv = otv[['inn', 'okr', 'otv', 'sch_name']]
+    return otv
+
+def get_fio():
+    fio = pd.read_excel(path_to_docs + 'ФИО+СНИЛС.xlsx').rename(columns={'СНИЛС': 'snils', 'ФИО': 'fio'})
+    return fio
+
+def to_double(row, f_name):
+    if type(row[f_name]) is float:
+        return row[f_name] 
+    t = str(row[f_name]).split(',')
+    if t[0] == 'nan':
+        return 0.0
+    return float(t[0]+'.'+t[1])
+
+def load_data(file):
+    df = pd.read_excel(path_to_data + file + '.xlsx')
+    df.stv = df.apply(lambda row: to_double(row, 'stv'), axis=1)
+    sample = df[(df.type == 'Основное место работы') | (df.type == 'Внутреннее совместительство')]
+    sums = sample.iloc[:, [0,1,2,3]].groupby(['inn', 'snils']).sum().reset_index().groupby('snils').max().reset_index()
+    sums = pd.merge(sums, sample[sample.type == 'Основное место работы'], how='left', on=['inn', 'snils'])
+    sums = sums[(sums.stv >= 1.0) & (sums.status == 'Работа') & (sums.day == 1)]
+    sums = sums[['inn', 'snils', 'sum_x', 'stv', 'job']].drop_duplicates(['inn', 'snils', 'sum_x'])
+    sums = sums.rename(columns = {'inn_y': 'inn', 'sum_x': 'sum_' + file, 'job':'job_' + file, 'stv': 'stv_' + file})
+    return sums
+
+def create_res(months):
+    res = 0
+    for i in range(len(months)):
+        if i == 0:
+            res = load_data(months[i])
+        else:
+            res = pd.merge(res, load_data(months[i]), how='outer', on=['inn', 'snils'])
+    return res
+
+def load_data_all(file):
+    df = pd.read_excel(path_to_data + file + '.xlsx')
+    df.stv = df.apply(lambda row: to_double(row, 'stv'), axis=1)
+    sample = df[~(df.type == 'Внешнее совместительство')]
+    sums = sample.iloc[:, [0,1,2,3]].groupby(['inn', 'snils']).sum().reset_index().groupby('snils').max().reset_index()
+    sums = pd.merge(sums, sample[sample.type == 'Основное место работы'], how='left', on=['inn', 'snils'])
+    sums = sums[['inn', 'snils', 'sum_x', 'stv', 'job']].drop_duplicates(['inn', 'snils', 'sum_x'])
+    sums = sums.rename(columns = {'inn': 'inn', 'sum_x': 'sum_' + file, 'job':'job_' + file, 'stv': 'stv_' + file})
+    return sums
+
+
+def create_full_res(months):
+    res = 0
+    for i in range(len(months)):
+        if i == 0:
+            res = load_data_all(months[i])
+        else:
+            res = pd.merge(res, load_data_all(months[i]), how='outer', on=['inn', 'snils'])
+    return res
+
+
+def print_df(df, name):
+    wb = Workbook()
+    ws = wb.active
+    for  i in df_to_row(df, header=True):
+        ws.append(i)
+    wb.save(name + '.xlsx')
+
+def szp(row, months):
+    # if pd.isna(row['job_' + months[-1]]):
+    #     return np.nan
+    cnt = 0
+    sum_szp = 0
+    for month in months:
+        if pd.isna(row['sum_' + month]):
+            continue
+        cnt += 1
+        sum_szp += row['sum_' + month]
+    # if row['inn'] == 9715217689 and row['snils'] == '173-260-871 66':
+    #     print(cnt, sum_szp)
+    if cnt == 0:
+        return np.nan
+    return sum_szp / cnt
+
+def szp_ped(row, months):
+    sum_szp = 0
+    cnt = 0
+    for month in months:
+        if row['job_' + month] in ped:
+            cnt += 1
+            sum_szp += row['sum_' + month]
+    if cnt == 0 or pd.isna(row['job_' + months[-1]]):
+        return np.nan
+    return sum_szp / cnt
+
+
+def szp_teach(row, months):
+    sum_szp = 0
+    cnt = 0
+    for month in months:
+        if row['job_' + month] in ['Учитель']:
+            cnt += 1
+            sum_szp += row['sum_' + month]
+    if cnt == 0 or row['job_' + months[-1]] != 'Учитель':
+        return np.nan
+    return sum_szp / cnt
+
+def szp_vosp(row, months):
+    sum_szp = 0
+    cnt = 0
+    for month in months:
+        if row['job_' + month] in vosp:
+            cnt += 1
+            sum_szp += row['sum_' + month]
+    if cnt == 0 or row['job_' + months[-1]] not in vosp:
+        return np.nan
+    return sum_szp / cnt
+
+def sum_fot(row, months):
+    sum = 0
+    for month in months:
+        if pd.isna(row['sum_' + month]) == False:
+            sum += row['sum_' + month]
+    return sum
+
+def load_groups(file):
+    df = pd.read_excel(path_to_docs + file + '.xlsx', sheet_name='УГД')
+    ped = df[df['Педагогический'] == 1]['Должность'].tolist()
+    isp = df[df['Работники непосредственно осуществляющие и обеспечивающие основной учебно-вспомогательный процесс во взаимодействии с детьми'] == 1]['Должность'].tolist()
+    aup = df[df['УГД'] == 'Административно-управленческий персонал']['Должность'].to_list()
+    return ped, isp, aup
